5d621a51b9e2d1ab98c7845fb52d8f4056b65b9b
gw31415 <24710985+gw31415@users.noreply.github.com>
2026-04-28 10:14:51 +0900

    Fix: Fixed an issue where calling this immediately after starting Vim caused an error. However, the layout is broken.

--- lua/fzyselect-nui-opener.lua
+++ lua/fzyselect-nui-opener.lua
@@ -18,18 +18,27 @@ function _G._fzyselect_nui_opener()
 		position = '50%',
 	}
 	popup:mount()
+	local winid = popup.winid
+	local make_center = function()
+		popup.border:set_text('top', vim.b.opts.prompt, 'center')
+		popup:update_layout {
+			size = {
+				height = vim.api.nvim_win_get_height(winid),
+				width = vim.api.nvim_win_get_width(winid),
+			},
+			position = '50%',
+		}
+	end
+	local callback = nil
+	callback = function()
+		local ok, _ = pcall(make_center)
+		if callback and not ok then
+			vim.defer_fn(callback, 100)
+		end
+	end
 	vim.api.nvim_create_autocmd('TextChanged', {
 		buffer = 0,
-		callback = function()
-			popup.border:set_text('top', vim.b.opts.prompt, 'center')
-			popup:update_layout {
-				size = {
-					height = vim.api.nvim_win_get_height(0),
-					width = vim.api.nvim_win_get_width(0),
-				},
-				position = '50%',
-			}
-		end,
+		callback = callback,
 	})
 	popup:on(event.BufLeave, function()
 		popup:unmount()
